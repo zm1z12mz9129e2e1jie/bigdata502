7d2141b192105a2ae18468d2ee8ae58adb98cb52
kang <tkrldi12@naver.com>
2019-09-20 12:17:43 +0900

    테스트2

--- Animal.java
+++ Animal.java
@@ -4,7 +4,16 @@ public class Animal {
 	public void play1() {
 		System.out.println("¸Û¸Û");
 	}
-	
+	public Animal() {
+		// TODO Auto-generated constructor stub
+	}@Override
+	protected Object clone() throws CloneNotSupportedException {
+		// TODO Auto-generated method stub
+		return super.clone();
+	}@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
 
 
 	public static void main(String[] args) {
