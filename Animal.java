710f3f64ff18d923c0381f4d4849fe11bfa5ad46
kang <tkrldi12@naver.com>
2019-09-20 12:14:46 +0900

    animal 파일 수정

--- Animal.java
+++ Animal.java
@@ -1,13 +1,16 @@
 
 public class Animal {
 	
-	public void play() {
+	public void play1() {
 		System.out.println("¸Û¸Û");
 	}
+	
+
 
 	public static void main(String[] args) {
 		Animal a = new Animal();
-		a.play();
+		a.play1();
+		
 	}
 
 }
